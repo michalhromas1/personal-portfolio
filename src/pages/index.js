61dca0a392737929f60ff50b2c2a26e04acd54bc
Michal Hromas <michalhromas1@gmail.com>
2020-04-27 17:42:02 +0200

    Add hero content, Add fluidity mixins

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,19 +1,18 @@
 import React from "react"
-import { useIntl } from "gatsby-plugin-intl"
+import Hero from "./../components/sections/hero"
 import "../styles/styles.scss"
 //import SEO from "../components/seo"
 
 const IndexPage = () => {
-  const intl = useIntl()
   return (
     <div>
       {/* <SEO title="Home" /> */}
-      {intl.formatMessage({ id: "nav.contact" })}
-      <div
+      {/* <div
         dangerouslySetInnerHTML={{
           __html: intl.formatMessage({ id: "about.desc" }),
         }}
-      ></div>
+      ></div> */}
+      <Hero />
     </div>
   )
 }
