1aea57b950ad720a0e69659a4e30d7a9c166d737
Michal Hromas <michalhromas1@gmail.com>
2020-05-19 16:57:50 +0200

    Various improvements and fixes
    - Add animations
    - Make notification box-shadow responsive
    - Update translations
    - Update browser compatibility
       - Add font smoothing - fixes boldness differences
       - Update hero font-size

--- src/components/sections/hero.js
+++ src/components/sections/hero.js
@@ -3,55 +3,63 @@ import { useIntl, Link } from "gatsby-plugin-intl"
 import Name from "./../../images/svg/name.svg"
 import Person from "./../../images/svg/person.svg"
 import Dot from "./../../images/svg/dot.svg"
+import Fade from "react-reveal/Fade"
 
 const Hero = () => {
   const intl = useIntl()
+  const animationDelay = 200
 
   return (
     <section className="hero container" id="hero">
       <div className="hero__content">
-        <h1 className="main-title hero__title">
-          <div className="main-title__greeting">
-            {intl.formatMessage({ id: "hero.greeting" })}
-          </div>
-          <div className="main-title__name-wrapper">
+        <Fade right cascade duration={1000} delay={animationDelay}>
+          <h1 className="main-title hero__title">
+            <div className="main-title__greeting">
+              {intl.formatMessage({ id: "hero.greeting" })}
+            </div>
+            <div className="main-title__name-wrapper">
+              <img
+                alt={intl.formatMessage({ id: "hero.name--alt" })}
+                className="main-title__name"
+                src={Name}
+              />
+            </div>
+            <div className="main-title__jobs">
+              <div className="main-title__job main-title__job--left">
+                {intl.formatMessage({ id: "hero.job.developer" })}
+              </div>
+              <div
+                className="main-title__jobs-divider"
+                style={{ backgroundImage: `url('${Dot}')` }}
+              ></div>
+              <div className="main-title__job main-title__job--right">
+                {intl.formatMessage({ id: "hero.job.designer" })}
+              </div>
+            </div>
+          </h1>
+        </Fade>
+
+        <Fade left duration={2000} delay={animationDelay}>
+          <div className="hero__person-wrapper">
             <img
-              alt={intl.formatMessage({ id: "hero.name--alt" })}
-              className="main-title__name"
-              src={Name}
+              alt={intl.formatMessage({ id: "hero.person--alt" })}
+              className="hero__person"
+              src={Person}
             />
           </div>
-          <div className="main-title__jobs">
-            <div className="main-title__job main-title__job--left">
-              {intl.formatMessage({ id: "hero.job.developer" })}
-            </div>
-            <div
-              className="main-title__jobs-divider"
-              style={{ backgroundImage: `url('${Dot}')` }}
-            ></div>
-            <div className="main-title__job main-title__job--right">
-              {intl.formatMessage({ id: "hero.job.designer" })}
-            </div>
+        </Fade>
+
+        <Fade right duration={2000} delay={animationDelay}>
+          <div className="hero__button-wrapper">
+            <Link
+              title={intl.formatMessage({ id: "hero.cta.title" })}
+              className="hero__button btn smooth-scroll"
+              to="/#main-footer"
+            >
+              {intl.formatMessage({ id: "hero.cta.text" })}
+            </Link>
           </div>
-        </h1>
-
-        <div className="hero__person-wrapper">
-          <img
-            alt={intl.formatMessage({ id: "hero.person--alt" })}
-            className="hero__person"
-            src={Person}
-          />
-        </div>
-
-        <div className="hero__button-wrapper">
-          <Link
-            title={intl.formatMessage({ id: "hero.cta.title" })}
-            className="hero__button btn smooth-scroll"
-            to="/#main-footer"
-          >
-            {intl.formatMessage({ id: "hero.cta.text" })}
-          </Link>
-        </div>
+        </Fade>
       </div>
 
       <Link
@@ -59,12 +67,14 @@ const Hero = () => {
         className="hero__learn-more learn-more smooth-scroll"
         to="/#about"
       >
-        <div className="learn-more__line-wrapper">
-          <div className="learn-more__line"></div>
-        </div>
-        <div className="learn-more__line-wrapper">
-          <div className="learn-more__line"></div>
-        </div>
+        <Fade top duration={1800} delay={animationDelay}>
+          <div className="learn-more__line-wrapper">
+            <div className="learn-more__line"></div>
+          </div>
+          <div className="learn-more__line-wrapper">
+            <div className="learn-more__line"></div>
+          </div>
+        </Fade>
       </Link>
     </section>
   )
--- src/components/sections/nav.js
+++ src/components/sections/nav.js
@@ -1,9 +1,11 @@
 import React, { useState } from "react"
 import { useIntl, Link } from "gatsby-plugin-intl"
+import Fade from "react-reveal/Fade"
 
 const Nav = () => {
   const [active, toggleMenu] = useState(false)
   const intl = useIntl()
+  const animationDelay = 200
 
   const urls = ["/#skills", "/#portfolio", "/#main-footer"]
   const links = []
@@ -19,33 +21,35 @@ const Nav = () => {
   })
 
   return (
-    <nav className="main-nav" role="navigation">
-      <ul className={`nav-content ${active ? "nav-content--active" : ""}`}>
-        {links.map((link, idx) => (
-          <li className="nav-content__item" key={idx}>
-            <Link
-              title={link.title}
-              className="nav-content__link smooth-scroll"
-              to={link.url}
-            >
-              {link.text}
-            </Link>
-          </li>
-        ))}
-      </ul>
+    <Fade right duration={2000} delay={animationDelay}>
+      <nav className="main-nav" role="navigation">
+        <ul className={`nav-content ${active ? "nav-content--active" : ""}`}>
+          {links.map((link, idx) => (
+            <li className="nav-content__item" key={idx}>
+              <Link
+                title={link.title}
+                className="nav-content__link smooth-scroll"
+                to={link.url}
+              >
+                {link.text}
+              </Link>
+            </li>
+          ))}
+        </ul>
 
-      <button
-        className={`main-nav__burger nav-burger ${
-          active ? "nav-burger--active" : ""
-        }`}
-        onClick={() => toggleMenu(!active)}
-        title={intl.formatMessage({ id: `nav.burger--title` })}
-      >
-        <div className="nav-burger__line"></div>
-        <div className="nav-burger__line"></div>
-        <div className="nav-burger__line"></div>
-      </button>
-    </nav>
+        <button
+          className={`main-nav__burger nav-burger ${
+            active ? "nav-burger--active" : ""
+          }`}
+          onClick={() => toggleMenu(!active)}
+          title={intl.formatMessage({ id: `nav.burger--title` })}
+        >
+          <div className="nav-burger__line"></div>
+          <div className="nav-burger__line"></div>
+          <div className="nav-burger__line"></div>
+        </button>
+      </nav>
+    </Fade>
   )
 }
 
