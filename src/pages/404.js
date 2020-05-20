07a0242d18ea513bf7324d8332ccf8f24b750cf7
Michal Hromas <michalhromas1@gmail.com>
2020-05-20 20:37:37 +0200

    Add favicon and SEO

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,7 +1,11 @@
 import React from "react"
 import "../styles/styles.scss"
-//import SEO from "../components/seo"
+import SEO from "../components/seo"
 
-const NotFoundPage = () => <div>{/* <SEO title="404: Not found" /> */}</div>
+const NotFoundPage = () => (
+  <div>
+    <SEO title="404 | Michal Hromas | Web Developer & Designer" />
+  </div>
+)
 
 export default NotFoundPage
